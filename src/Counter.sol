bb4592a731ef77d6dbdf0372b964890ae7999c77
Sagiri00 <sp72650oy@gmail.com>
2026-04-07 07:20:34 +0000

    feat: add owner-based access control and constructor

--- src/Counter.sol
+++ src/Counter.sol
@@ -2,10 +2,14 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
+    address public owner;
     uint256 public number;
-
+    constructor () {
+        owner= msg.sender;
+    }
     function setNumber(uint256 newNumber) public {
-        number = newNumber;
+        require(msg.sender==owner ,"Hey!You are not the Owner");
+        number=newNumber;
     }
 
     function increment() public {
