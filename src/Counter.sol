aed671e17cc4dccab2dc6a30e646a10eecd9150a
Sagiri00 <sp72650oy@gmail.com>
2026-04-07 08:27:42 +0000

    style: run forge fmt

--- src/Counter.sol
+++ src/Counter.sol
@@ -4,12 +4,14 @@ pragma solidity ^0.8.13;
 contract Counter {
     address public owner;
     uint256 public number;
-    constructor () {
-        owner= msg.sender;
+
+    constructor() {
+        owner = msg.sender;
     }
+
     function setNumber(uint256 newNumber) public {
-        require(msg.sender==owner ,"Hey!You are not the Owner");
-        number=newNumber;
+        require(msg.sender == owner, "Hey!You are not the Owner");
+        number = newNumber;
     }
 
     function increment() public {
